6b6ce04cd26e8543a22faccdb7daf9ff05dead0f
Fefel76 <leredusiracus@gmail.com>
2023-01-08 17:30:43 +0100

    data.py pour collecter des photos toutes les x secondes et x fois

--- data.py
+++ data.py
@@ -5,21 +5,30 @@ import sys
 
 delai=1
 boucle=5
+rafale=2
 if len(sys.argv) > 2:
     delai = int(sys.argv[2])
 
 if len(sys.argv) > 3:
     boucle = int(sys.argv[3])
 
+if len(sys.argv) > 4:
+    rafale = int(sys.argv[4])
+
 # initialisation de la caméra
 src, name, visu, fps, record= init_config()
-cap, width, height = init_CAM(src=src, fps=fps, name=name)
+
 
 for t in range(boucle):
-    frame = read_frame(cap,name)
+    cap, width, height = init_CAM(src=src, fps=fps, name=name)
+    frame = read_frame(cap, name)
     photo(frame=frame, name=name)
+    for i in range(int(fps)):  #lecture de 15 frames sans photo
+        frame = read_frame(cap, name)
+    frame = read_frame(cap, name)
+    photo(frame=frame, name=name)
+    cap.release()
     time.sleep(delai)
 
 
-cap.release()
 cv2.destroyAllWindows()
